5a0ad7de7a36aec81ddeb0b4a1409403e9a07702
Kelvin Bonni <kelvin.bonni@trilogy.com>
2024-08-08 22:12:04 +0200

    Initial commit

---
+++ src/main/java/com/valr/api/ValrApiApplication.java
@@ -0,0 +1,13 @@
+package com.valr.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValrApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valr/api/ValrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValrApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
