692cf1bc15d0243e6be9fa964f6b0e7e743e840f
Kelvin Bonni <kelvin.bonni@trilogy.com>
2024-08-12 01:11:27 +0200

    Adds unit tests

--- src/main/java/com/valr/api/service/order/OrderServiceImpl.java
+++ src/main/java/com/valr/api/service/order/OrderServiceImpl.java
@@ -1,13 +1,11 @@
 package com.valr.api.service.order;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.valr.api.model.order.CurrencyPair;
 import com.valr.api.model.order.Order;
 import com.valr.api.model.order.OrderBook;
 import com.valr.api.model.order.Side;
 import com.valr.api.model.trade.Trade;
 import com.valr.api.utils.IdGenerator;
-import com.valr.api.utils.Locker;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -26,49 +23,45 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class OrderServiceImpl implements OrderService{
     @Autowired
-    Locker locker;
-    @Autowired
     IdGenerator idGenerator;
 
-    private Map<CurrencyPair, OrderBook> orderBooks = new HashMap<>();
-    private Map<CurrencyPair, List<Trade>> recentTrades = new HashMap<>();
+    private EnumMap<CurrencyPair, OrderBook> orderBooks = new EnumMap<>(CurrencyPair.class);
+    private EnumMap<CurrencyPair, List<Trade>> recentTrades = new EnumMap<>(CurrencyPair.class);
 
     @Override
     public OrderBook getOrderBooks(CurrencyPair pair) {
-        return locker.withReadLock(() -> orderBooks.getOrDefault(pair, createOrderBooks()));
+        return orderBooks.getOrDefault(pair, createOrderBooks());
     }
 
     @Override
     public List<Trade> getTradeHistory(CurrencyPair pair) {
-        return locker.withReadLock(() -> recentTrades.getOrDefault(pair, new ArrayList<>()));
+        return recentTrades.getOrDefault(pair, new ArrayList<>());
     }
 
     @Override
     public Order limitOrder(Order order) {
-        return locker.withWriteLock(() -> {
-            OrderBook orderBook = orderBooks.computeIfAbsent(order.getCurrencyPair(), k -> createOrderBooks());
-            orderBook.setSequenceNumber(orderBook.getSequenceNumber() + 1);
+        OrderBook orderBook = orderBooks.computeIfAbsent(order.getCurrencyPair(), k -> createOrderBooks());
+        orderBook.setSequenceNumber(orderBook.getSequenceNumber() + 1);
 
-            double remainingQuantity;
-            if(Side.BUY == order.getSide()){
-                remainingQuantity = matchOrder(order, orderBook.getAsks(), orderBook, order.getCurrencyPair());
-                if (remainingQuantity > 0.0) {
-                    // Add remaining quantity as a new buy order
-                    Order remainingOrder = new Order(idGenerator.generate(), order.getSide(), remainingQuantity, order.getPrice(), order.getCurrencyPair(), order.getTimestamp());
-                    orderBook.setBids(new HashSet<>(Set.of(remainingOrder)));
-                }
-            }else if(Side.SELL == order.getSide()){
-                remainingQuantity = matchOrder(order, orderBook.getBids(), orderBook, order.getCurrencyPair());
-                if (remainingQuantity > 0.0) {
-                    // Add remaining quantity as a new sell order
-                    Order remainingOrder = new Order(idGenerator.generate(), order.getSide(), remainingQuantity, order.getPrice(), order.getCurrencyPair(), order.getTimestamp());
-                    orderBook.setAsks(new HashSet<>(Set.of(remainingOrder)));
-                }
+        double remainingQuantity;
+        if(Side.BUY == order.getSide()){
+            remainingQuantity = matchOrder(order, orderBook.getAsks(), orderBook, order.getCurrencyPair());
+            if (remainingQuantity > 0.0) {
+                // Add remaining quantity as a new buy order
+                Order remainingOrder = new Order(idGenerator.generate(), order.getSide(), remainingQuantity, order.getPrice(), order.getCurrencyPair(), order.getTimestamp());
+                orderBook.setBids(new HashSet<>(Set.of(remainingOrder)));
             }
+        }else if(Side.SELL == order.getSide()){
+            remainingQuantity = matchOrder(order, orderBook.getBids(), orderBook, order.getCurrencyPair());
+            if (remainingQuantity > 0.0) {
+                // Add remaining quantity as a new sell order
+                Order remainingOrder = new Order(idGenerator.generate(), order.getSide(), remainingQuantity, order.getPrice(), order.getCurrencyPair(), order.getTimestamp());
+                orderBook.setAsks(new HashSet<>(Set.of(remainingOrder)));
+            }
+        }
 
-            orderBook.setLastChange(Instant.now());
-            return order;
-        });
+        orderBook.setLastChange(Instant.now());
+        return order;
     }
 
     private double matchOrder(
@@ -77,7 +70,7 @@ public class OrderServiceImpl implements OrderService{
             OrderBook orderBook,
             CurrencyPair pair
     ) {
-        List<Trade> trades = recentTrades.computeIfAbsent(pair, k -> new ArrayList<>());
+        List<Trade> trades = new ArrayList<>(recentTrades.computeIfAbsent(pair, k -> new ArrayList<>()));
 
         final double[] remainingQuantity = {order.getQuantity()};
 
@@ -111,7 +104,7 @@ public class OrderServiceImpl implements OrderService{
                     }
                 })
                 .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+                .toList();
 
         if (!ordersToRemove.isEmpty()) {
             oppositeOrders.removeAll(ordersToRemove);
--- src/test/java/com/valr/api/service/order/OrderServiceTest.java
+++ src/test/java/com/valr/api/service/order/OrderServiceTest.java
@@ -1,263 +1,154 @@
-//package com.valr.api.service.order;
-//
-//import com.valr.api.model.order.CurrencyPair;
-//import com.valr.api.model.order.Order;
-//import com.valr.api.model.order.OrderBook;
-//import com.valr.api.model.order.Side;
-//import com.valr.api.model.trade.Trade;
-//import com.valr.api.utils.IdGenerator;
-//import com.valr.api.utils.Locker;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.test.util.ReflectionTestUtils;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//import java.time.Instant;
-//import java.util.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class OrderServiceTest {
-//
-//    @InjectMocks
-//    OrderServiceImpl orderService;
-//    @Mock(strictness = Mock.Strictness.LENIENT)
-//    IdGenerator idGenerator;
-//    @Mock(strictness = Mock.Strictness.LENIENT)
-//    Locker locker;
-//
-//    private Map<CurrencyPair, OrderBook> orderBooks;
-//    Map<CurrencyPair, List<Trade>> recentTrades;
-//    CurrencyPair currencyPair;
-//
-//    private final String firstGeneratedId = UUID.fromString("65e0a581-971b-4520-974b-d0535c91e744").toString();
-//
-//    @BeforeEach
-//    public void setUp() {
-//        orderBooks = new HashMap<>();
-//        recentTrades = new HashMap<>();
-//        currencyPair = CurrencyPair.BTCZAR;
-//
-//        Map<CurrencyPair, OrderBook> orderBookMap = new HashMap<>();
-//        orderBookMap.put(currencyPair, new OrderBook());
-//
-//        ReflectionTestUtils.setField(orderService, "orderBooks", orderBookMap);
-//
-////        when(locker.withReadLock(any())).th
-//        when(idGenerator.generate()).thenReturn(firstGeneratedId);
-//    }
-//
-//    @Test
-//    public void testGetOrderBookReturnsNewOrderBookIfNotPresent() {
-//        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
-//        assertThat(orderBook.getAsks()).isEmpty();
-//        assertThat(orderBook.getBids()).isEmpty();
-//    }
-//
-//    @Test
-//    public void testPlaceLimitOrderAddsNewBuyOrder() {
-//        Order order = new Order(
-//                UUID.randomUUID().toString(),
-//                Side.BUY,
-//                1.0,
-//                50000L,
-//                currencyPair,
-//                Instant.now()
-//        );
-//
-//        orderService.limitOrder(order);
-//        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
-//        assertThat(orderBook.getAsks()).isEmpty();
-//        assertThat(orderBook.getBids()).hasSize(1);
-//        assertThat(orderBook.getBids().iterator().next()).isEqualTo(copyOrder(firstGeneratedId, order));
-//
-//        assertThat(orderService.getTradeHistory(currencyPair)).isEmpty();
-//    }
-//
-//    public Order copyOrder(String id, Order order){
-//        return new Order(id, order.getSide(), order.getQuantity(), order.getPrice(), order.getCurrencyPair(), order.getTimestamp());
-//    }
-//
-//    @Test
-//    public void testPlaceLimitOrderAddsNewSellOrder() {
-//        Order order = new Order(
-//                UUID.randomUUID().toString(),
-//                Side.SELL,
-//                1.0,
-//                50000L,
-//                currencyPair,
-//                Instant.now()
-//        );
-//
-//        orderService.limitOrder(order);
-//        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
-//        assertThat(orderBook.getBids()).isEmpty();
-//        assertThat(orderBook.getAsks()).hasSize(1);
-//        assertThat(orderBook.getAsks().iterator().next()).isEqualTo(copyOrder(firstGeneratedId, order));
-//
-//        assertThat(orderService.getTradeHistory(currencyPair)).isEmpty();
-//    }
-//
-//    @Test
-//    public void testMatchOrderMatchesAndRemovesOrdersCorrectly() {
-//        Order sellOrder = new Order(
-//                UUID.randomUUID().toString(),
-//                Side.SELL,
-//                1.0,
-//                50000L,
-//                currencyPair,
-//                Instant.now()
-//        );
-//
-//        orderService.limitOrder(sellOrder);
-//
-//        Order buyOrder = new Order(
-//                UUID.randomUUID().toString(),
-//                Side.BUY,
-//                1.0,
-//                50000L,
-//                currencyPair,
-//
-//                Instant.now()
-//        );
-//
-//        orderService.limitOrder(buyOrder);
-//
-//        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
-//        assertThat(orderBook.getBids()).isEmpty();
-//        assertThat(orderBook.getAsks()).isEmpty();
-//
-//        List<Trade> trades = orderService.getTradeHistory(currencyPair);
-//        assertThat(trades).hasSize(1);
-//        assertThat(trades.get(0).getPrice()).isEqualTo(50000);
-//        assertThat(trades.get(0).getQuantity()).isEqualTo(1.0);
-//    }
-//
-//    @Test
-//    public void testMatchOrderPartialMatch() {
-//        Order sellOrder = new Order(
-//                UUID.randomUUID().toString(),
-//                Side.SELL,
-//                1.0,
-//                50000L,
-//                currencyPair,
-//                Instant.now()
-//        );
-//
-//        orderService.limitOrder(sellOrder);
-//
-//        Order buyOrder = new Order(
-//                UUID.randomUUID().toString(),
-//                Side.BUY,
-//                0.5,
-//                50000L,
-//                currencyPair,
-//                Instant.now()
-//        );
-//
-//        orderService.limitOrder(buyOrder);
-//
-//        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
-//        assertThat(orderBook.getBids()).isEmpty();
-//        assertThat(orderBook.getAsks()).hasSize(1);
-//        Order generatedOrder = orderBook.getAsks().iterator().next();
-//        assertThat(generatedOrder).isEqualTo(new Order(
-//                firstGeneratedId,
-//                Side.SELL,
-//                0.5,
-//                50000L,
-//                currencyPair,
-//                generatedOrder.getTimestamp()
-//        ));
-//
-//        List<Trade> trades = orderService.getTradeHistory(currencyPair);
-//        assertThat(trades).hasSize(1);
-//        assertThat(trades.get(0).getPrice()).isEqualTo(50000);
-//        assertThat(trades.get(0).getQuantity()).isEqualTo(0.5);
-//    }
-//
-//    @Test
-//    public void testOrderMatchingOverfillsDemand() {
-//        OrderBook initialOrderBook = new OrderBook(
-//                new TreeSet<>(Set.of(
-//                        new Order(UUID.randomUUID().toString(), Side.SELL, 0.5, 1000000L, currencyPair),
-//                        new Order(UUID.randomUUID().toString(), Side.SELL, 1.0, 1100000L, currencyPair)
-//                )),
-//                Instant.now(),
-//                1L
-//        );
-//
-//        orderService.getOrderBooks().put(currencyPair, initialOrderBook);
-//
-//        Order overfillOrder = new Order(UUID.randomUUID().toString(), Side.BUY, 2.0, 1200000L, currencyPair);
-//        Order resultOrder = orderService.limitOrder(overfillOrder);
-//
-//        assertThat(resultOrder.getQuantity()).isEqualTo(2.0);
-//
-//        OrderBook updatedOrderBook = orderService.getOrderBooks(currencyPair);
-//        assertThat(updatedOrderBook.getAsks()).isEmpty();
-//        assertThat(updatedOrderBook.getBids()).hasSize(1);
-//        Order generatedOrder = updatedOrderBook.getBids().iterator().next();
-//        assertThat(generatedOrder).isEqualTo(new Order(
-//                firstGeneratedId,
-//                Side.BUY,
-//                0.5,
-//                1200000L,
-//                currencyPair,
-//                generatedOrder.getTimestamp()
-//        ));
-//
-//        List<Trade> trades = orderService.getTradeHistory(currencyPair);
-//        assertThat(trades).hasSize(2);
-//        assertThat(trades.get(0).getPrice()).isEqualTo(1000000);
-//        assertThat(trades.get(0).getQuantity()).isEqualTo(0.5);
-//        assertThat(trades.get(1).getPrice()).isEqualTo(1100000);
-//        assertThat(trades.get(1).getQuantity()).isEqualTo(1.0);
-//    }
-//
-//    @Test
-//    public void testOrderMatchingDoesNotMatchOnLastOrderPrices() {
-//        OrderBook initialOrderBook = new OrderBook(
-//                new TreeSet<>(Set.of(
-//                        new Order("1", Side.SELL, 0.5, 1000000L, currencyPair),
-//                        new Order("2", Side.SELL, 1.0, 1100000L, currencyPair)
-//                )),
-//                Instant.now(),
-//                1L
-//        );
-//
-//        orderService.getOrderBooks().put(currencyPair, initialOrderBook);
-//
-//        Order overfillOrder = new Order("4", Side.BUY, 2.0, 1000500L, currencyPair);
-//        Order resultOrder = orderService.limitOrder(overfillOrder);
-//        assertThat(resultOrder.getId()).isNotNull();
-//        assertThat(resultOrder.getQuantity()).isEqualTo(2.0);
-//
-//        OrderBook updatedOrderBook = orderService.getOrderBooks(currencyPair);
-//        assertThat(updatedOrderBook.getAsks()).hasSize(2);
-//        assertThat(updatedOrderBook.getBids()).hasSize(1);
-//        Order generatedOrder = updatedOrderBook.getBids().iterator().next();
-//        assertThat(generatedOrder).isEqualTo(new Order(
-//                firstGeneratedId,
-//                Side.BUY,
-//                1.5,
-//                1000500L,
-//                currencyPair,
-//                generatedOrder.getTimestamp()
-//        ));
-//
-//        List<Trade> trades = orderService.getTradeHistory(currencyPair);
-//        assertThat(trades).hasSize(1);
-//        assertThat(trades.get(0).getPrice()).isEqualTo(1000000);
-//        assertThat(trades.get(0).getQuantity()).isEqualTo(0.5);
-//    }
-//}
+package com.valr.api.service.order;
+
+import com.valr.api.model.order.CurrencyPair;
+import com.valr.api.model.order.Order;
+import com.valr.api.model.order.OrderBook;
+import com.valr.api.model.order.Side;
+import com.valr.api.model.trade.Trade;
+import com.valr.api.utils.IdGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+import java.util.*;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest {
+
+    @InjectMocks
+    OrderServiceImpl orderService;
+    @Mock(strictness = Mock.Strictness.LENIENT)
+    IdGenerator idGenerator;
+
+    CurrencyPair currencyPair;
+
+    private final String firstGeneratedId = UUID.fromString("65e0a581-971b-4520-974b-d0535c91e744").toString();
+
+    @BeforeEach
+    public void setUp() {
+        currencyPair = CurrencyPair.BTCZAR;
+
+        EnumMap<CurrencyPair, OrderBook> orderBookMap = new EnumMap<>(CurrencyPair.class);
+        orderBookMap.put(currencyPair, new OrderBook());
+        ReflectionTestUtils.setField(orderService, "orderBooks", orderBookMap);
+
+        EnumMap<CurrencyPair, List<Trade>> recentTradeMap = new EnumMap<>(CurrencyPair.class);
+        recentTradeMap.put(currencyPair, List.of(new Trade()));
+        ReflectionTestUtils.setField(orderService, "recentTrades", recentTradeMap);
+
+        when(idGenerator.generate()).thenReturn(firstGeneratedId);
+    }
+
+    @Test
+    void testGetOrderBookReturnsNewOrderBookIfNotPresent() {
+        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
+        assertThat(orderBook.getAsks()).isEmpty();
+        assertThat(orderBook.getBids()).isEmpty();
+    }
+
+    @Test
+    void testPlaceLimitOrderAddsNewBuyOrder() {
+        Order order = new Order(
+                UUID.randomUUID().toString(),
+                Side.BUY,
+                1.0,
+                50000L,
+                currencyPair,
+                Instant.now()
+        );
+
+        orderService.limitOrder(order);
+        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
+        assertThat(orderBook.getAsks()).isEmpty();
+        assertThat(orderBook.getBids()).hasSize(1);
+    }
+
+    @Test
+    void testPlaceLimitOrderAddsNewSellOrder() {
+        Order order = new Order(
+                UUID.randomUUID().toString(),
+                Side.SELL,
+                1.0,
+                50000L,
+                currencyPair,
+                Instant.now()
+        );
+
+        orderService.limitOrder(order);
+        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
+        assertThat(orderBook.getBids()).isEmpty();
+        assertThat(orderBook.getAsks()).hasSize(1);
+    }
+
+    @Test
+    void testMatchOrderMatchesAndRemovesOrdersCorrectly() {
+        Order sellOrder = new Order(
+                UUID.randomUUID().toString(),
+                Side.SELL,
+                1.0,
+                50000L,
+                currencyPair,
+                Instant.now()
+        );
+
+        orderService.limitOrder(sellOrder);
+
+        Order buyOrder = new Order(
+                UUID.randomUUID().toString(),
+                Side.BUY,
+                1.0,
+                50000L,
+                currencyPair,
+                Instant.now()
+        );
+
+        orderService.limitOrder(buyOrder);
+
+        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
+        assertThat(orderBook.getBids()).isEmpty();
+        assertThat(orderBook.getAsks()).isEmpty();
+
+        List<Trade> trades = orderService.getTradeHistory(currencyPair);
+        assertThat(trades).hasSize(1);
+    }
+
+    @Test
+    void testMatchOrderPartialMatch() {
+        Order sellOrder = new Order(
+                UUID.randomUUID().toString(),
+                Side.SELL,
+                1.0,
+                50000L,
+                currencyPair,
+                Instant.now()
+        );
+
+        orderService.limitOrder(sellOrder);
+
+        Order buyOrder = new Order(
+                UUID.randomUUID().toString(),
+                Side.BUY,
+                0.5,
+                50000L,
+                currencyPair,
+                Instant.now()
+        );
+
+        orderService.limitOrder(buyOrder);
+
+        OrderBook orderBook = orderService.getOrderBooks(currencyPair);
+        assertThat(orderBook.getBids()).isEmpty();
+        assertThat(orderBook.getAsks()).hasSize(1);
+
+        List<Trade> trades = orderService.getTradeHistory(currencyPair);
+        assertThat(trades).hasSize(1);
+    }
+}
